7bda9a318ae54371a5bd997213bd83e1c5725078
Juan López <juan07alberto@gmail.com>
2019-10-30 13:29:12 -0600

    Functional

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ instance = accessToDB.accesSql('localhost', 'root', '', 'acdb')
 instance.insertAC('G81', 25, 0)
 
 line = serial.Serial('/dev/ttyACM0', 9600)
-
-while True:
-    read = str(line.readline())
-    if(read):
-        temp = float(read[4:9])
-        state = int(read[2])
-        instance.insertRegForHour('G81', state, state)
-
+try:    
+    while True:
+        read = str(line.readline())
+        if(read):
+            temp = float(read[4:9])
+            state = int(read[2])
+            instance.insertRegForHour('G81', state, state)
+            
+except KeyboardInterrupt:
+    print('\nSaliendo...')
+    
 
 
 
