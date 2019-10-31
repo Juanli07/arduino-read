940ab6779eb2e84809e65e98c5f8bccb05fa7d49
Juan López <juan07alberto@gmail.com>
2019-10-30 18:56:46 -0600

    Websocket add

---
+++ serverws.py
@@ -0,0 +1,27 @@
+from db import accessToDB
+import socket, json, time, os
+
+access = accessToDB.accesSql("localhost", "root", "", "acdb")
+my_socket = socket.socket()
+my_socket.bind((socket.gethostname(), 8000))
+my_socket.listen(5)
+print("[*] socket online in {}".format(socket.gethostname()))
+
+try:
+    while True:
+        con, addr =my_socket.accept()
+        print('[*] New connection established from {}'.format(addr))
+        data = access.select("ac")
+        send_data = json.dumps(data)
+
+        if(con.send(send_data.encode())):
+            print('[*] {}'.format(con.recv(1024).decode()))
+            con.close()
+        else:
+            print('[!] Error sending data')
+
+except KeyboardInterrupt:
+    print('\nSaliendo..')
+finally:
+    time.sleep(5)
+    os.system ("clear")
